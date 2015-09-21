08a85e1015c9c8acfa9a6afd5e863d1c167a3e81
steedos <zhuangjianguo@hotoadeMacBook-Pro.local>
2015-09-21 15:22:07 +0800

    default build to production

--- tasks/utils.js
+++ tasks/utils.js
@@ -25,7 +25,7 @@ module.exports.replace = function (str, patterns) {
 };
 
 module.exports.getEnvName = function () {
-    return argv.env || 'development';
+    return argv.env || 'production';
 };
 
 module.exports.getElectronVersion = function () {
